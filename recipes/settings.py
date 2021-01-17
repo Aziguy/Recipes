b483bd38863e43cfb5c4fbd7404de55544b3206b
Aziguy <monsieur_hernandez@hotmail.com>
2021-01-17 15:23:00 +0100

    started deployement

--- recipes/settings.py
+++ recipes/settings.py
@@ -167,8 +167,8 @@ SITE_ID = 1
 # Email sending
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'someone@gmail.com'
-EMAIL_HOST_PASSWORD = 'azerty0123@#'
+EMAIL_HOST_USER = "mandatairessm@gmail.com"
+EMAIL_HOST_PASSWORD = "uatdrsm2020"
 EMAIL_USE_TLS = True
 
 # Linkedin Authentication Setting
